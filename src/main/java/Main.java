130c8a7260a9bc4017f58b8383c9a70d70fe5c9e
Legic <Legic2007@yandex.ru>
2023-11-10 00:07:58 +0300

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,19 +4,18 @@ public class Main {
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
     public static void main(String[] args) throws InterruptedException {
-        int routesCount = 100;
-        int routeLength = 10;
+        int routesCount = 1000;
+        int routeLength = 100;
 
         Thread threadMaxValue = new Thread(() -> { // Поток - поиск лидера
             int stepCount = 0;
-            while (!Thread.interrupted()) {
-                stepCount++;
-
-                synchronized (sizeToFreq) {
+            synchronized (sizeToFreq) {
+                while (!Thread.interrupted()) {
+                    stepCount++;
                     try {
                         sizeToFreq.wait(); // ждем сообщение от потока
                     } catch (InterruptedException e) {
-                        e.printStackTrace();
+                        return;
                     }
                     System.out.printf("Шаг %d: Самое частое количество повторений: %s (встречается %s раз)%n",
                             stepCount, keyMapByMaxValue(sizeToFreq), sizeToFreq.get(keyMapByMaxValue(sizeToFreq)));
@@ -31,21 +30,21 @@ public class Main {
                 int countR = countCharOccurrences(routeString, 'R');// считаем количество символов 'R' в маршруте
                 synchronized (sizeToFreq) {
                     fillingMap(countR, sizeToFreq); // заполняем Map
+                    System.out.println(routeString + " -> " + countR);
                     sizeToFreq.notify(); // сообщаем потоку
 
-                    System.out.println(routeString + " -> " + countR);
                 }
             });
             threadCounter.start();
             threadCounter.join(); // ждем завершения потоков до вывода итогов
         }
-
         threadMaxValue.interrupt();
 
         System.out.printf("Самое частое количество повторений: %s (встречается %s раз)%n",
                 keyMapByMaxValue(sizeToFreq), sizeToFreq.get(keyMapByMaxValue(sizeToFreq)));
         System.out.printf("Остальные значения (%d): %n", sizeToFreq.size());
         sizeToFreq.forEach((key, value) -> System.out.printf(" - %s (встречается %s раз)%n", key, value));
+
     }
 
     // Функция генерирования маршрута
@@ -66,14 +65,14 @@ public class Main {
     }
 
     // Функция поиска max значения в Map (отдает ключ)
-    public static synchronized int keyMapByMaxValue(Map<Integer, Integer> map) {
+    public static int keyMapByMaxValue(Map<Integer, Integer> map) {
         Optional<Map.Entry<Integer, Integer>> maxEntry = map.entrySet().stream()
                 .max(Comparator.comparing(Map.Entry::getValue));
         return maxEntry.get().getKey();
     }
 
     // Функция заполнения Map ключами и значениями (критическая секция синхронизации)
-    public static synchronized void fillingMap(int countR, Map<Integer, Integer> sizeToFreq) {
+    public static void fillingMap(int countR, Map<Integer, Integer> sizeToFreq) {
         if (sizeToFreq.containsKey(countR)) {
             int keyCount = sizeToFreq.get(countR) + 1; // ключ есть - увеличиваем значение на 1
             sizeToFreq.put(countR, keyCount);
